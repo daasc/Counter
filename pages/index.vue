46965b743a09a76838f611dd7fecf5964a6ba1bb
Paulo Sobrinho Ferreira <paulo.sobrinho@opabox.com.br>
2022-02-15 21:29:35 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ counter }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  computed: {
+    counter() {
+      return this.$store.state.counter.counter
+    },
+  },
+}
+</script>
